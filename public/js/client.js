6535a792b298f8da7615065da3733b6749fbdc14
jasontsukamoto <jasontsukamoto@gmail.com>
2015-07-03 14:49:25 -1000

    basic functionality

--- public/js/client.js
+++ public/js/client.js
@@ -5,33 +5,126 @@
   var SOCKET_RECONNECTING = 'reconnecting';
   var SOCKET_RECONNECT = 'reconnect';
   var SOCKET_ERROR = 'error';
+  var SOCKET_USER_MESSAGE = 'user message';
+  var SOCKET_USER_REGISTRATION = 'user registration';
+  var SOCKET_USER_CONNECT = 'user connect';
+  var SOCKET_USER_ONLINE = 'user online';
+var SOCKET_USER_OFFLINE = 'user offline';
+
+
+
+  var SYSTEM = 'System';
 
 
   var socket = io.connect(SERVER_ADDRESS);
-console.log($);
-console.log(io);
-  // socket.on(SOCKET_CONNECT, function() {
-  //   console.log('I connected');
-  // });
 
-  // socket.on(SOCKET_DISCONNECT, function() {
-  //   console.log('disonnected');
-  // });
+  socket.on(SOCKET_CONNECT, function() {
+    message(SYSTEM, 'Connected to ' + SERVER_ADDRESS);
+  });
 
-  // socket.on(SOCKET_RECONNECTING, function() {
-  //   console.log('reconnecting');
-  // });
+  socket.on(SOCKET_DISCONNECT, function() {
+    message(SYSTEM, 'Disconnected from ' + SERVER_ADDRESS);
+  });
 
-  // socket.on(SOCKET_RECONNECT, function() {
-  //   console.log('reconnected');
-  // });
+  socket.on(SOCKET_RECONNECTING, function() {
+    message(SYSTEM, 'Attempting to reconnect to ' + SERVER_ADDRESS);
+  });
 
-  // socket.on(SOCKET_ERROR, function() {
-  //   console.log('error');
-  // });
+  socket.on(SOCKET_RECONNECT, function() {
+    message(SYSTEM, 'Reconnected to ' + SERVER_ADDRESS);
+  });
 
-  // socket.on(SOCKET_CONNECT, function() {
-  //   console.log('I connected');
-  // });
+  socket.on(SOCKET_ERROR, function(err) {
+    if (err !== undefined) {
+      message(SYSTEM, err);
+    } else {
+      message(SYSTEM, 'An unknown error occured.');
+    }
+  });
+
+  socket.on(SOCKET_USER_MESSAGE, function(from, userMessage) {
+    message(from, userMessage);
+  });
+
+  socket.on(SOCKET_USER_CONNECT, function(nickname) {
+    message('', nickname);
+  });
+
+  socket.on(SOCKET_USER_ONLINE, function(nickname) {
+    userList(nickname);
+  });
+
+    socket.on(SOCKET_USER_ONLINE, function(nickname) {
+    userList(nickname);
+  });
+
+
+  function message(from, message) {
+    var newMessage = $('<p>');
+    var fromTag = $('<b>', {
+      html : from
+    });
+    var messageTag = $('<span>', {
+      html : message
+    });
+    newMessage.append(fromTag);
+    newMessage.append(messageTag);
+
+    $('#chatlog')
+      .append(newMessage);
+    $('#chatlog')
+      .get(0).scrollTop = Infinity;
+  }
+
+  function userList(nickname) {
+    var onlineNickname = $('#onlineNickname');
+    var onlineUser = $('<li>', {
+      html : nickname
+    });
+    onlineNickname.append(onlineUser);
+  }
+
+  $('#messageForm').submit(function(event) {
+    event.preventDefault();
+    var messageField = $('#message');
+    var theMessage = messageField.val();
+    //add my message to the chatlog
+    message('me', theMessage);
+    //send my message to the server
+    socket.emit(SOCKET_USER_MESSAGE, theMessage);
+    //clear the message input field
+    messageField.val('');
+    //keep the page from refreshing
+  });
+
+  $('#registration_form').submit(function(event) {
+    event.preventDefault();
+    var nickname = $('#nickname').val();
+    //send nickname to server
+    socket.emit(SOCKET_USER_REGISTRATION, nickname, function(available) {
+      //if nickname is available
+      //  go to chatroom
+      if (available) {
+        changeStateToChatRoom();
+      } else {
+        //show error
+        $('#nickname_error').text('Nickname already in use');
+      }
+    });
+  });
+
+  //manage state
+  var registration = $('#registration');
+  var chatroom = $('#chatroom');
+
+  //default state
+  //show registration
+  //hide chatroom
+  chatroom.hide();
+
+  function changeStateToChatRoom() {
+    chatroom.show();
+    registration.hide();
+  }
 
 })();
--- server.js
+++ server.js
@@ -1,10 +1,51 @@
 var PORT = 9000;
 var SOCKET_CONNECTION = 'connection';
+var SOCKET_USER_MESSAGE = 'user message';
+var SOCKET_USER_REGISTRATION = 'user registration';
+var SERVER_USER = 'Server';
+var SOCKET_USER_CONNECT = 'user connect';
+var SOCKET_USER_ONLINE = 'user online';
+var SOCKET_DISCONNECT = 'disconnect';
+var SOCKET_USER_OFFLINE = 'user offline';
+
 
 var socketIO = require('socket.io');
 
 var server = socketIO.listen(PORT);
+var nicknames = {};
 
-server.sockets.on(SOCKET_CONNECTION, function() {
-  console.log('connected');
+server.sockets.on(SOCKET_CONNECTION, function(socket) {
+
+  socket.on(SOCKET_USER_MESSAGE, function(message) {
+    //broadcast this message to all connected servers
+    socket.broadcast.emit(SOCKET_USER_MESSAGE, socket.nickname, message);
+  });
+
+  socket.on(SOCKET_USER_REGISTRATION, function(nickname, callback) {
+    if(nicknames.hasOwnProperty(nickname) || nickname.length === 0) {
+      //it's taken
+      callback(false);
+    } else {
+      //available! add it
+      nicknames[nickname] = nickname;
+
+      //assign the nickname to the socket
+      socket.nickname = nickname;
+
+      //broadcast the new user
+      socket.broadcast.emit(SOCKET_USER_CONNECT, nickname + ' has connected.');
+      socket.emit(SOCKET_USER_CONNECT, nickname + ' has connected.');
+      socket.broadcast.emit(SOCKET_USER_ONLINE, nickname)
+      socket.emit(SOCKET_USER_ONLINE, nickname);
+
+
+      //successful registration
+      callback(true);
+    }
+  });
+
+  server.sockets.on(SOCKET_DISCONNECT, function() {
+    socket.broadcast.emit(SOCKET_USER_OFFLINE, socket.nickname +' has disconnected');
+  });
 });
+
